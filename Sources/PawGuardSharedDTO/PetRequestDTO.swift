ad9c18c3b7831b6ca47db60bed3260d8a590b098
Eldar Tutnjic <eldar.tutnjic@symvaro.com>
2023-11-27 00:16:18 +0100

    pet breed and castrated aded

--- Sources/PawGuardSharedDTO/PetRequestDTO.swift
+++ Sources/PawGuardSharedDTO/PetRequestDTO.swift
@@ -10,19 +10,23 @@ import Foundation
 public struct PetRequestDTO: Codable {
     public let name: String
     public let species: String
+    public let petBreed: String
     public let sex: String
     public let dateOfBirth: String
     public let color: String
+    public let castratedSterilized: String
     public let microchipNumber: String
     public let microchipLocation: String
     public let imageURL: String
     
-    public init(name: String, species: String, sex: String, dateOfBirth: String, color: String, microchipNumber: String, microchipLocation: String, imageURL: String) {
+    public init(name: String, species: String, sex: String, petBreed: String, dateOfBirth: String, color: String, castratedSterilized: String,  microchipNumber: String, microchipLocation: String, imageURL: String) {
         self.name = name
         self.species = species
+        self.petBreed = petBreed
         self.sex = sex
         self.dateOfBirth = dateOfBirth
         self.color = color
+        self.castratedSterilized = castratedSterilized
         self.microchipNumber = microchipNumber
         self.microchipLocation = microchipLocation
         self.imageURL = imageURL
