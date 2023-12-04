59afe6a9a67ecb1f3493f52ac180b1d83c92ca42
Eldar Tutnjic <eldar.tutnjic@symvaro.com>
2023-12-04 18:01:41 +0100

    PasswordChnageDTO refactor

--- Sources/PawGuardSharedDTO/PasswordChangeDTO.swift
+++ Sources/PawGuardSharedDTO/PasswordChangeDTO.swift
@@ -9,14 +9,13 @@ import Foundation
 
 public struct PasswordChangeDTO: Codable {
     public let error: Bool
-    public var reason: String? = nil
-    public let oldPassword: String
-    public let newPassword: String
-    
-    public init(error: Bool, reason: String? = nil, oldPassword: String, newPassword: String) {
+    public var reason: String?
+    public var successMessage: String?
+
+    public init(error: Bool, reason: String? = nil, successMessage: String? = nil) {
         self.error = error
         self.reason = reason
-        self.oldPassword = oldPassword
-        self.newPassword = newPassword
+        self.successMessage = successMessage
     }
 }
+
