4ad7f0d110300840c07562805786b393828f9eb9
Eldar Tutnjic <eldar.tutnjic@symvaro.com>
2023-12-04 15:21:18 +0100

    Added error in passwordchangedto

--- Sources/PawGuardSharedDTO/PasswordChangeDTO.swift
+++ Sources/PawGuardSharedDTO/PasswordChangeDTO.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 public struct PasswordChangeDTO: Codable {
+    public let error: Bool
     public let oldPassword: String
     public let newPassword: String
     
-    public init(oldPassword: String, newPassword: String) {
+    public init(error: Bool, oldPassword: String, newPassword: String) {
+        self.error = error
         self.oldPassword = oldPassword
         self.newPassword = newPassword
     }
