3108d8c74260c66ca54abce1a7d1d2fe40208d5a
Eldar Tutnjic <eldar.tutnjic@symvaro.com>
2023-12-24 21:43:04 +0100

    Changed to public

--- Sources/PawGuardSharedDTO/ChangeLogDTO/ChangeLogResponseDTO.swift
+++ Sources/PawGuardSharedDTO/ChangeLogDTO/ChangeLogResponseDTO.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public struct ChangeLogResponseDTO: Codable {
-    let id: UUID
-    let titleText: String
-    let descriptionText: String
-    let dateCreated: Date?
+    public let id: UUID
+    public let titleText: String
+    public let descriptionText: String
+    public let dateCreated: Date?
     
     public init(id: UUID, titleText: String, descriptionText: String, dateCreated: Date?) {
         self.id = id
