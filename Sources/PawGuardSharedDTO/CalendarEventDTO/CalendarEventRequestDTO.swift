2e23fd8d2b6f20d9b8480528903e59abbfad35db
Eldar Tutnjic <eldartutnjic@gmail.com>
2024-03-02 12:38:16 +0100

    Added eventEndDate to DTO

--- Sources/PawGuardSharedDTO/CalendarEventDTO/CalendarEventRequestDTO.swift
+++ Sources/PawGuardSharedDTO/CalendarEventDTO/CalendarEventRequestDTO.swift
@@ -10,12 +10,14 @@ import Foundation
 public struct CalendarEventRequestDTO: Codable {
     public let eventTitle: String
     public let eventStartDate: Date
+    public let eventEndDate: Date
     public let eventRecurrence: Int
     public let eventCareNoteID: String
     
-    public init(eventTitle: String, eventStartDate: Date, eventRecurrence: Int, eventCareNoteID: String) {
+    public init(eventTitle: String, eventStartDate: Date, eventEndDate: Date, eventRecurrence: Int, eventCareNoteID: String) {
         self.eventTitle = eventTitle
         self.eventStartDate = eventStartDate
+        self.eventEndDate = eventEndDate
         self.eventRecurrence = eventRecurrence
         self.eventCareNoteID = eventCareNoteID
     }
--- Sources/PawGuardSharedDTO/CalendarEventDTO/CalendarEventResponseDTO.swift
+++ Sources/PawGuardSharedDTO/CalendarEventDTO/CalendarEventResponseDTO.swift
@@ -11,13 +11,15 @@ public struct CalendarEventResponseDTO: Codable {
     public let id: UUID
     public let eventTitle: String
     public let eventStartDate: Date
+    public let eventEndDate: Date
     public let eventRecurrence: Int
     public let eventCareNoteID: String
     
-    public init(id: UUID, eventTitle: String, eventStartDate: Date, eventRecurrence: Int, eventCareNoteID: String) {
+    public init(id: UUID, eventTitle: String, eventStartDate: Date, eventEndDate: Date, eventRecurrence: Int, eventCareNoteID: String) {
         self.id = id
         self.eventTitle = eventTitle
         self.eventStartDate = eventStartDate
+        self.eventEndDate = eventEndDate
         self.eventRecurrence = eventRecurrence
         self.eventCareNoteID = eventCareNoteID
     }
